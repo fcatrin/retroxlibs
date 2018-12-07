d2b02dafb77435c238f4abbd91d65d12cd6f8ebd
Franco Catrin <fcatrin@gmail.com>
2018-12-07 18:45:10 -0300

    Fix label alignment on gamepad info

--- RetroXUtils/src/retrobox/utils/GamepadLayoutManager.java
+++ RetroXUtils/src/retrobox/utils/GamepadLayoutManager.java
@@ -1,8 +1,10 @@
 package retrobox.utils;
 
 import android.app.Activity;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.ViewGroup.LayoutParams;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.TextView;
 import xtvapps.core.AndroidFonts;
@@ -16,8 +18,6 @@ public class GamepadLayoutManager {
 		BTN_A, BTN_B, BTN_X, BTN_Y, BTN_SELECT, BTN_START 
 	}
 
-	private enum Pivot {START, END, MIDDLE}
-
 	private int buttonWidth;
 	private int buttonHeight;
 
@@ -28,18 +28,18 @@ public class GamepadLayoutManager {
 	public GamepadLayoutManager(Activity activity, int imageResourceId, int[] labelResourceIds) {
 		this.activity = activity;
 		
-		labelBoxes[ButtonId.BTN_L2.ordinal()] = new ButtonLabelBox(labelResourceIds[0], 92, 8, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_L1.ordinal()] = new ButtonLabelBox(labelResourceIds[1], 92, 40, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_R2.ordinal()] = new ButtonLabelBox(labelResourceIds[2], 420, 8, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_R1.ordinal()] = new ButtonLabelBox(labelResourceIds[3], 420, 40, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_L3.ordinal()] = new ButtonLabelBox(labelResourceIds[4], 175, 204, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_R3.ordinal()] = new ButtonLabelBox(labelResourceIds[5], 334, 204, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_A.ordinal()]  = new ButtonLabelBox(labelResourceIds[6], 451, 127, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_B.ordinal()]  = new ButtonLabelBox(labelResourceIds[7], 415, 163, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_X.ordinal()]  = new ButtonLabelBox(labelResourceIds[8], 415, 92, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_Y.ordinal()]  = new ButtonLabelBox(labelResourceIds[9], 379, 127, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_SELECT.ordinal()] = new ButtonLabelBox(labelResourceIds[10], 225, 126, Pivot.MIDDLE, Pivot.MIDDLE);
-		labelBoxes[ButtonId.BTN_START.ordinal()]  = new ButtonLabelBox(labelResourceIds[11], 285, 126, Pivot.MIDDLE, Pivot.MIDDLE);
+		labelBoxes[ButtonId.BTN_L2.ordinal()] = new ButtonLabelBox(labelResourceIds[0], 92, 8);
+		labelBoxes[ButtonId.BTN_L1.ordinal()] = new ButtonLabelBox(labelResourceIds[1], 92, 40);
+		labelBoxes[ButtonId.BTN_R2.ordinal()] = new ButtonLabelBox(labelResourceIds[2], 420, 8);
+		labelBoxes[ButtonId.BTN_R1.ordinal()] = new ButtonLabelBox(labelResourceIds[3], 420, 40);
+		labelBoxes[ButtonId.BTN_L3.ordinal()] = new ButtonLabelBox(labelResourceIds[4], 175, 204);
+		labelBoxes[ButtonId.BTN_R3.ordinal()] = new ButtonLabelBox(labelResourceIds[5], 334, 204);
+		labelBoxes[ButtonId.BTN_A.ordinal()]  = new ButtonLabelBox(labelResourceIds[6], 451, 127);
+		labelBoxes[ButtonId.BTN_B.ordinal()]  = new ButtonLabelBox(labelResourceIds[7], 415, 163);
+		labelBoxes[ButtonId.BTN_X.ordinal()]  = new ButtonLabelBox(labelResourceIds[8], 415, 92);
+		labelBoxes[ButtonId.BTN_Y.ordinal()]  = new ButtonLabelBox(labelResourceIds[9], 379, 127);
+		labelBoxes[ButtonId.BTN_SELECT.ordinal()] = new ButtonLabelBox(labelResourceIds[10], 225, 126);
+		labelBoxes[ButtonId.BTN_START.ordinal()]  = new ButtonLabelBox(labelResourceIds[11], 285, 126);
 		
 		for (ButtonLabelBox labelBox : labelBoxes) {
 			View v = activity.findViewById(labelBox.labelResourceId);
@@ -84,16 +84,12 @@ public class GamepadLayoutManager {
 	public class ButtonLabelBox {
 		int x;
 		int y;
-		Pivot hPivot;
-		Pivot vPivot;
 		int labelResourceId;
 		
-		public ButtonLabelBox(int labelResourceId, int x, int y, Pivot hPivot, Pivot vPivot) {
+		public ButtonLabelBox(int labelResourceId, int x, int y) {
 			this.labelResourceId = labelResourceId;
 			this.x = x;
 			this.y = y;
-			this.hPivot = hPivot;
-			this.vPivot = vPivot;
 		}
 		
 		public void setLabel(Activity activity, String label) {
@@ -107,30 +103,20 @@ public class GamepadLayoutManager {
 		}
 
 		public void layout(Activity activity, float ratioWidth, float ratioHeight) {
-			View v = activity.findViewById(labelResourceId);
+			TextView v = (TextView)activity.findViewById(labelResourceId);
 			if (v == null) return;
 
-			int top = 0;
-			int left = 0;
-			int w = v.getWidth();
-			int h = v.getHeight();
+			int w = buttonWidth * 4;
+			int h = buttonHeight;
 			
-			switch (hPivot) {
-			case START  : left = x; break;
-			case END    : left = x - w; break;
-			case MIDDLE : left = x - (w/2); break;
-			}
-			switch (vPivot) {
-			case START  : top = y; break;
-			case END    : top = y - h; break;
-			case MIDDLE : top = y - (h/2); break;
-			}
+			LayoutParams textLayoutParams = v.getLayoutParams();
+			textLayoutParams.width = w;
+			textLayoutParams.height = h;
 			
-			left += buttonWidth / 2;
-			top  += buttonHeight / 2;
-			
-			v.setTranslationX((int) (left * ratioWidth));
-			v.setTranslationY((int) (top  * ratioHeight));
+			v.setGravity(Gravity.CENTER);
+
+			v.setTranslationX((int) (x * ratioWidth + buttonWidth / 2 - buttonWidth*2));
+			v.setTranslationY((int) (y * ratioHeight));
 		}
 		
 	}
